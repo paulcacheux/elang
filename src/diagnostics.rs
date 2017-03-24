d1bc33d9eb10964c864b66453e3165f705550db0
Paul CACHEUX <paulcacheux@gmail.com>
2017-03-24 10:11:06 +0100

    Better diagnostics with arrows

--- src/diagnostics.rs
+++ src/diagnostics.rs
@@ -2,18 +2,16 @@ use lexer::{LexicalError, Token};
 use lalrpop_util::ParseError;
 
 pub fn print_diagnostic(input: &str, error: ParseError<usize, Token, LexicalError>) {
-    let line_starts = build_line_starts(input);
-
     let mut error_lines = Vec::new();
 
     match error {
         ParseError::InvalidToken { location } => {
-            error_lines.extend(get_lines(input, &line_starts, vec![location]));
+            error_lines.extend(get_lines(input, (location, location + 1)));
             println!("Invalid token.");
         },
         ParseError::UnrecognizedToken { token, expected } => {
             if let Some((start, tok, end)) = token {
-                error_lines.extend(get_lines(input, &line_starts, vec![start, end]));
+                error_lines.extend(get_lines(input, (start, end)));
                 println!("Unrecognized token: {:?}.", tok);
             } else {
                 println!("Unexpected eof.");
@@ -21,40 +19,37 @@ pub fn print_diagnostic(input: &str, error: ParseError<usize, Token, LexicalErro
             println!("Expected one of: {}", expected.join(", "));
         },
         ParseError::ExtraToken { token: (start, tok, end) } => {
-            error_lines.extend(get_lines(input, &line_starts, vec![start, end]));
+            error_lines.extend(get_lines(input, (start, end)));
             println!("Extra token: {:?}.", tok);
         },
         ParseError::User { error: LexicalError { msg, pos } } => {
-            error_lines.extend(get_lines(input, &line_starts, vec![pos]));
+            error_lines.extend(get_lines(input, (pos, pos + 1)));
             println!("{}", msg);
         }
     }
 
-    for (i, line) in error_lines {
-        println!("{}: {}", i, line);
+    for (i, (line, arrow)) in error_lines {
+        println!("{:<5}: {}", i+1, line);
+        println!("       {}", arrow);
     }
 }
 
-fn build_line_starts(input: &str) -> Vec<usize> {
-    let mut starts = vec![0];
-    for (i, c) in input.char_indices() {
-        if c == '\n' {
-            starts.push(i);
-        }
-    }
-    starts
-}
+fn get_lines<'a>(input: &'a str, span: (usize, usize)) -> Vec<(usize, (&'a str, String))> {
+    let mut arrow = String::with_capacity(input.len());
 
-fn get_lines<'a>(input: &'a str, starts: &Vec<usize>, positions: Vec<usize>) -> Vec<(usize, &'a str)> {
-    let mut lines = Vec::new();
-
-    for pos in positions {
-        let real_start = &0;
-        let (line, index) = starts.iter().take_while(|&&i| i < pos).enumerate().last().unwrap_or((0, real_start));
-        let line_str = &input[*index+1..starts[line+1]];
-        lines.push((line, line_str));
+    for (i, c) in input.chars().enumerate() {
+        arrow.push(match c {
+            '\n' => '\n',
+            _ if span.0 <= i && i < span.1 => '^',
+            _ => ' ',
+        });
     }
 
-    lines.dedup_by_key(|item| item.0);
-    lines
+    input.lines()
+        .zip(arrow.lines().map(String::from))
+        .enumerate()
+        .filter(|&(_, (_, ref arrow))| {
+            arrow.contains('^')
+        })
+        .collect()
 }
