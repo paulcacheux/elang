6097ddce1c687b2d02df12d9133ee7808322be06
Paul CACHEUX <paulcacheux@gmail.com>
2017-04-05 18:13:36 +0200

    Added c io lib

--- runner.sh
+++ runner.sh
@@ -3,5 +3,7 @@ objpath="$1.o"
 expath="$1.out"
 ./target/debug/elang $1 -O -t=llvm -o $outpath
 /usr/local/opt/llvm@4/bin/llc -O3 -filetype=obj $outpath -o $objpath
-clang $objpath -o $expath
+clang $objpath ./elang_examples/io.o -o $expath
 ./$expath
+rm $outpath
+rm $objpath
