c62c922a7e334d6c0b73a3e889e7c09ef01014e3
Paul CACHEUX <paulcacheux@gmail.com>
2017-04-05 20:46:15 +0200

    Cleaned the structure of the project

--- runner.sh
+++ runner.sh
@@ -1,10 +1,10 @@
 outpath="$1.ll"
 objpath="$1.o"
-expath="$1.out"
+expath="utils/$(basename $1).out"
 ./target/debug/elang $1 -O -t=llvm -o $outpath
 /usr/local/opt/llvm@4/bin/llc -O3 -filetype=obj $outpath -o $objpath
-clang -c ./elang_examples/io.c -o ./elang_examples/io.o
-clang $objpath ./elang_examples/io.o -o $expath
+clang -c ./utils/io.c -o ./utils/io.o
+clang $objpath ./utils/io.o -o $expath
 ./$expath
 rm $outpath
 rm $objpath
