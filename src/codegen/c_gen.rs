d86ff2921bf747a2852ab393548a487c383550d6
Paul CACHEUX <paulcacheux@gmail.com>
2017-04-06 21:43:08 +0200

    Just nothing

--- src/codegen/c_gen.rs
+++ src/codegen/c_gen.rs
@@ -114,8 +114,6 @@ fn gen_expr<F: Write>(f: &mut F, expr: ir::Expression) -> io::Result<()> {
                 IntGreaterEqual | DoubleGreaterEqual => ">=",
                 IntEqual | DoubleEqual | BoolEqual => "==",
                 IntNotEqual | DoubleNotEqual | BoolNotEqual => "!=",
-                // BoolLogicalAnd => "&&",
-                // BoolLogicalOr => "||",
             };
 
             write!(f, "temp_{} {} temp_{}", lhs.id, op, rhs.id)
