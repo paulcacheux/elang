2ecf61b6013ab5c57bfcbde2f9dc1c012803506b
Paul CACHEUX <paulcacheux@gmail.com>
2017-04-11 17:40:20 +0200

    Renamed examples dir and modified runner.sh

--- runner.sh
+++ runner.sh
@@ -3,8 +3,8 @@ optpath="$1.opt.ll"
 objpath="$1.o"
 expath="utils/$(basename $1).out"
 ./target/debug/elang $1 -O -t=llvm -o $outpath
-/usr/local/opt/llvm@4/bin/opt -O3 -S $outpath -o $optpath
-/usr/local/opt/llvm@4/bin/llc -filetype=obj $optpath -o $objpath
+opt -O3 -S $outpath -o $optpath
+llc -filetype=obj $optpath -o $objpath
 clang $objpath -o $expath
 #./$expath
 rm $optpath
